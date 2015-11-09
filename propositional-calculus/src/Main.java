b9d26918028113e3e6aba40fbe5608586c484872
Gálffy Tamás <ezittgtx@gmail.com>
2015-11-09 18:54:44 +0100

    ErrorVisitor

--- propositional-calculus/src/Main.java
+++ propositional-calculus/src/Main.java
@@ -2,13 +2,23 @@ import java.util.Scanner;
 
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.DefaultErrorStrategy;
+import org.antlr.v4.runtime.ANTLRErrorStrategy;
 import org.antlr.v4.runtime.ANTLRInputStream;
 
-class Visitor extends logicBaseVisitor<Integer>{
+class ErrorVisitor extends logicBaseVisitor<Integer>{
+	private int errors = 0;
+	
 	public Integer visitExpr(logicParser.ExprContext ctx) 
-	{ 
-		System.out.println("Oh hey, an expr! " + ctx.getText());
+	{
+		if(ctx.exception != null)
+			errors++;
+		
 		return visitChildren(ctx); 
+	}
+	
+	public int getErrors() {
+		return errors; 
 	}
 }
 
@@ -25,9 +35,14 @@ public class Main {
 			CharStream stream = new ANTLRInputStream(line);
 			logicLexer lexer = new logicLexer(stream);
 			CommonTokenStream tokens = new CommonTokenStream(lexer); 
+			
 			logicParser parser = new logicParser(tokens);
-			logicVisitor<Integer> visitor = new Visitor();
+			ErrorVisitor visitor = new ErrorVisitor();
 			visitor.visit(parser.line());
+			
+			if(visitor.getErrors() != 0) {
+				System.out.printf("Found %d error(s)\n", visitor.getErrors());
+			}
 		}
 		
 		sc.close();
