08b31db2d08eee905f4c389404e2d3c8fb0b6f03
Gálffy Tamás <ezittgtx@gmail.com>
2015-11-16 19:27:04 +0100

    Nagyon alap tagadás, yo!

--- propositional-calculus/src/Main.java
+++ propositional-calculus/src/Main.java
@@ -47,6 +47,7 @@ public class Main {
 	public static final int RULE_ATOM = 0;
 	public static final int RULE_NEGATION = 1;
 	public static final int RULE_BINARY_OP = 2;
+	public static final int RULE_EXPR = 3;
 	
 	public static int classifyNode(ParseTree node) {
 		ParserRuleContext ctx = (ParserRuleContext)node.getPayload();
@@ -55,8 +56,10 @@ public class Main {
 			return RULE_NEGATION; 
 		else if(ctx.getToken(logicParser.OP,0) != null)
 			return RULE_BINARY_OP; 
-		else 
+		else if(ctx.getToken(logicParser.ATOM, 0) != null)
 			return RULE_ATOM; 
+		else 
+			return RULE_EXPR; 
 	}
 	
 	public static List<ParseTree> extractNodes(ParseTree node) {
@@ -72,27 +75,69 @@ public class Main {
 		return ret; 
 	}
 	
+	public static List<Token> extractTokens(ParseTree node) {
+		List<Token> ret = new ArrayList<Token>();
+		
+		for(int i = 0; i < node.getChildCount(); i++)
+		{
+			ParseTree c = node.getChild(i);
+			if(c.getPayload() instanceof Token)
+				ret.add((Token)c.getPayload());
+		}
+		
+		return ret; 
+	}
+	
+	//((!(A & B)) | (!(A | B)))
+	//((!<A>) | (!<B>)|(!<A>) & (!<B>))#
+	
+	public static void negateNode(ParseTree node) {
+		System.out.print("(!<");
+		printNode(node);
+		System.out.print(">)");
+	}
+	
 	public static void printNode(ParseTree node) {
 		if(node.getPayload() instanceof ParserRuleContext) {
 			if(classifyNode(node) == RULE_NEGATION) {
 				ParseTree subnode = extractNodes(node).get(0);
 				
 				if(classifyNode(subnode) == RULE_BINARY_OP) {
-					//GO GO DESCARTES SAN!
+					List<Token> tokens = extractTokens(subnode);
+					List<ParseTree> nodes = extractNodes(subnode);
+					
+					Token operator = tokens.get(1);
+					
+					if(operator.getText().equals("&")) {
+						negateNode(nodes.get(0));
+						System.out.print(" | ");
+						negateNode(nodes.get(1));
+					}
+					else if(operator.getText().equals("|")) {
+						negateNode(nodes.get(0));
+						System.out.print(" & ");
+						negateNode(nodes.get(1));
+					}
+					else 
+						; //???!
 				}
 				else if(classifyNode(subnode) == RULE_NEGATION) {
 					//GO GO NEGATE SAN
+					for(int i = 0; i < node.getChildCount(); i++)
+						printNode(node.getChild(i));
 				}
 				else 
-					;//print children
+					for(int i = 0; i < node.getChildCount(); i++)
+						printNode(node.getChild(i));
 			}
-			
-			for(int i = 0; i < node.getChildCount(); i++)
-				printNode(node.getChild(i));
+			else {
+				for(int i = 0; i < node.getChildCount(); i++)
+					printNode(node.getChild(i));
+			}
 		}
 		else if(node.getPayload() instanceof Token) {
 			Token t = (Token)node.getPayload();
-			System.out.printf("%s", t.getText());
+			System.out.printf("%s ", t.getText());
 		}
 		else {
 			//???!
