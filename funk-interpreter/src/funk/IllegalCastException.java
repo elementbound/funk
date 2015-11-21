9deefec7f9fa4271b90342e7b09fbb54e023905f
Gálffy Tamás <ezittgtx@gmail.com>
2015-11-21 21:38:16 +0100

    Fixed IllegalCastException a bit

--- funk-interpreter/src/funk/IllegalCastException.java
+++ funk-interpreter/src/funk/IllegalCastException.java
@@ -1,24 +1,13 @@
 package funk;
 
 public class IllegalCastException extends Exception {
-	String message;
 	Type from;
 	Type to; 
 	
 	public IllegalCastException(Type from, Type to) {
+		super("Illegal cast from " + from + " to " + to);
+		
 		this.from = from;
 		this.to = to;
-		
-		StringBuilder strb = new StringBuilder();
-		strb.append("Illegal cast from ")
-			.append(from)
-			.append(" to ")
-			.append(to);
-		
-		this.message = strb.toString();
-	}
-	
-	public String what() {
-		return this.message;
 	}
 }
---
+++ funk-interpreter/src/funk/Main.java
@@ -0,0 +1,7 @@
+package funk;
+
+public class Main {
+	public static void main(String... args) throws IllegalCastException {
+		throw new IllegalCastException(Type.String, Type.Boolean);
+	}
+}
