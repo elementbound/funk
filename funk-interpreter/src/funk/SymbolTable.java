cd2c8498086ec47fb60e90ba20151f557caaaed3
iam346 <badogergo@gmail.com>
2015-11-28 16:24:33 +0100

    Table

---
+++ funk-interpreter/src/funk/SymbolTable.java
@@ -0,0 +1,20 @@
+package funk;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SymbolTable {
+	public Map<String, Object> table = new HashMap<>();
+	
+	public Object get(String key){
+		return table.get(key);
+	}
+	
+    public void set(String key, Object val){
+    	table.put(key, val);
+    }
+    
+    public boolean exists(String key){
+    	return table.containsKey(key);
+    }
+}
