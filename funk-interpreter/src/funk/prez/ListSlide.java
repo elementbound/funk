15c040638fc1ef9aa4a204cd4db29e701eaaca4d
Gálffy Tamás <ezittgtx@gmail.com>
2015-11-28 21:02:48 +0100

    Configurable slide classes

--- funk-interpreter/src/funk/prez/ListSlide.java
+++ funk-interpreter/src/funk/prez/ListSlide.java
@@ -7,14 +7,31 @@ import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JPanel;
 import javax.swing.BoxLayout;
 import javax.swing.SwingConstants;
 import java.awt.Component;
+import java.awt.Dimension;
 
 public class ListSlide {
 
 	private JFrame frame;
+	
+	private String title = "Page Title"; 
+	private List<String> bulletPointTexts = new ArrayList<>();
+	
+	public void setTitle(String title) {
+		this.title = title; 
+	}
+	
+	public void addBullet(String text) {
+		bulletPointTexts.add(text);
+	}
 
 	/**
 	 * Launch the application.
@@ -24,7 +41,13 @@ public class ListSlide {
 			public void run() {
 				try {
 					ListSlide window = new ListSlide();
-					window.frame.setVisible(true);
+					
+					window.setTitle("Attenshun!");
+					window.addBullet("Shit's gonna go down!");
+					window.addBullet("Right on this slide!");
+					window.addBullet("Brace yourselves!");
+					
+					window.show();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -33,21 +56,28 @@ public class ListSlide {
 	}
 
 	/**
-	 * Create the application.
-	 */
-	public ListSlide() {
-		initialize();
-	}
-
-	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void show() {
 		frame = new JFrame();
+		
+		frame.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				if(arg0.getKeyCode() == KeyEvent.VK_ENTER)
+					System.out.println("Next slide!");
+				else if(arg0.getKeyCode() == KeyEvent.VK_UP && arg0.isControlDown())
+					System.out.println("Fullscreen!");
+				else if(arg0.getKeyCode() == KeyEvent.VK_DOWN && arg0.isControlDown())
+					System.out.println("Windowed!");
+			}
+		});
+		
+		frame.setTitle("Funk Presentation");
 		frame.getContentPane().setBackground(Color.BLACK);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
 		
-		JLabel lblPageTitle = new JLabel("Page Title");
+		JLabel lblPageTitle = new JLabel(title);
 		lblPageTitle.setVerticalAlignment(SwingConstants.TOP);
 		lblPageTitle.setFont(new Font("Tahoma", Font.BOLD, 32));
 		lblPageTitle.setForeground(Color.WHITE);
@@ -59,32 +89,38 @@ public class ListSlide {
 		ohGodWhyPanel.setLayout(null);
 		
 		JPanel textPanel = new JPanel();
-		textPanel.setBounds(32, 0, 434, 222);
+		textPanel.setBounds(32, 16, 434, 222);
 		ohGodWhyPanel.add(textPanel);
 		textPanel.setBackground(Color.BLACK);
 		textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.Y_AXIS));
 		
-		JPanel bulletPointPanel = new JPanel();
-		bulletPointPanel.setForeground(new Color(255, 255, 255));
-		bulletPointPanel.setAlignmentY(Component.TOP_ALIGNMENT);
-		bulletPointPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-		bulletPointPanel.setBackground(Color.BLACK);
-		textPanel.add(bulletPointPanel);
-		bulletPointPanel.setLayout(new BoxLayout(bulletPointPanel, BoxLayout.X_AXIS));
+		for(String text : bulletPointTexts) {
+			System.out.printf("Added text: %s\n", text);
+			
+			JPanel bulletPointPanel = new JPanel();
+			bulletPointPanel.setForeground(new Color(255, 255, 255));
+			bulletPointPanel.setAlignmentY(Component.TOP_ALIGNMENT);
+			bulletPointPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+			bulletPointPanel.setBackground(Color.BLACK);
+			textPanel.add(bulletPointPanel);
+			bulletPointPanel.setLayout(new BoxLayout(bulletPointPanel, BoxLayout.X_AXIS));
+			
+			JLabel lblDot = new JLabel("\u2022 ");
+			lblDot.setFont(new Font("Tahoma", Font.PLAIN, 20));
+			lblDot.setForeground(Color.LIGHT_GRAY);
+			bulletPointPanel.add(lblDot);
+			
+			JLabel lblBulletPoint = new JLabel(text);
+			bulletPointPanel.add(lblBulletPoint);
+			lblBulletPoint.setForeground(Color.WHITE);
+			lblBulletPoint.setFont(new Font("Tahoma", Font.PLAIN, 20));
+			lblBulletPoint.setVerticalAlignment(SwingConstants.TOP);
+		}
 		
-		JLabel lblDot = new JLabel("\u2022 ");
-		lblDot.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblDot.setForeground(Color.LIGHT_GRAY);
-		bulletPointPanel.add(lblDot);
-		
-		JLabel lblBulletPoint = new JLabel("Bullet point");
-		bulletPointPanel.add(lblBulletPoint);
-		lblBulletPoint.setForeground(Color.WHITE);
-		lblBulletPoint.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblBulletPoint.setVerticalAlignment(SwingConstants.TOP);
 		frame.setBackground(Color.BLACK);
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 
 }
--- funk-interpreter/src/funk/prez/TitleSide.java
+++ funk-interpreter/src/funk/prez/TitleSide.java
@@ -7,13 +7,24 @@ import javax.swing.JLabel;
 import java.awt.BorderLayout;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+
+import javafx.scene.input.KeyCode;
+
 import java.awt.Color;
 import java.awt.Window.Type;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.BoxLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class TitleSide {
 
 	private JFrame frame;
 
+	private String title = "Page title";
+	private String sectitle = "Secondary title";
+	
 	/**
 	 * Launch the application.
 	 */
@@ -22,7 +33,11 @@ public class TitleSide {
 			public void run() {
 				try {
 					TitleSide window = new TitleSide();
-					window.frame.setVisible(true);
+					
+					window.setTitle("Funk Symbol Table Support");
+					window.setSecondaryTitle("Presentation now with 70% more plot twist!");
+					
+					window.show();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -30,34 +45,49 @@ public class TitleSide {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
-	public TitleSide() {
-		initialize();
+	public void setTitle(String title) {
+		this.title = title;
 	}
-
+	
+	public void setSecondaryTitle(String title) {
+		this.sectitle = title;
+	}
+	
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void show() {
 		frame = new JFrame();
+		frame.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				if(arg0.getKeyCode() == KeyEvent.VK_ENTER)
+					System.out.println("Next slide!");
+				else if(arg0.getKeyCode() == KeyEvent.VK_UP && arg0.isControlDown())
+					System.out.println("Fullscreen!");
+				else if(arg0.getKeyCode() == KeyEvent.VK_DOWN && arg0.isControlDown())
+					System.out.println("Windowed!");
+			}
+		});
+		
 		frame.setTitle("Funk Presentation");
 		frame.getContentPane().setBackground(Color.BLACK);
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JLabel lblPageTitle = new JLabel("Page Title");
+		JLabel lblPageTitle = new JLabel(title);
 		lblPageTitle.setForeground(Color.WHITE);
 		lblPageTitle.setBackground(Color.BLACK);
 		lblPageTitle.setFont(new Font("Tahoma", Font.BOLD, 32));
 		frame.getContentPane().add(lblPageTitle, BorderLayout.NORTH);
 		
-		JLabel lblSecondaryTitle = new JLabel("Secondary Title");
+		JLabel lblSecondaryTitle = new JLabel("    "+sectitle);
 		lblSecondaryTitle.setForeground(Color.LIGHT_GRAY);
 		lblSecondaryTitle.setFont(new Font("Tahoma", Font.PLAIN, 24));
 		lblSecondaryTitle.setVerticalAlignment(SwingConstants.TOP);
 		frame.getContentPane().add(lblSecondaryTitle, BorderLayout.WEST);
+		
+		frame.setVisible(true);
 	}
 
 }
