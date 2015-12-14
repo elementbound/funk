a13ae9a18c04340dcafa2877ba580579f71115cd
Gálffy Tamás <ezittgtx@gmail.com>
2015-12-14 23:38:51 +0100

    Revamped Colletion type - now uses List<Entry<>> instead of a LinkedHashMap Didn't feel like writing equals and hashCode for each type

--- funk-interpreter/src/funk/lang/types/Collection.java
+++ funk-interpreter/src/funk/lang/types/Collection.java
@@ -1,8 +1,8 @@
 package funk.lang.types;
 
+import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
 import java.util.LinkedHashMap;
 import funk.lang.StandardErrors;
@@ -11,14 +11,10 @@ import funk.lang.StandardErrors;
 import funk.lang.Object; 
 
 public class Collection extends Object {
-	//A Collection tulajdonk�ppen egy Map, csak meg van adva hogy milyen sorrendben tartja a kulcs-�rt�k p�rjait
-	//Els�re �gy ahogy belepakolod Funk-b�l, azt�n lehet rendezgetni meg ilyesmi
-	Map<Object,Object> collection = new LinkedHashMap<Object,Object>();
-	int iterator=0;
-	//A Collection tart mag�ban egy mutat�t is, iter�lgat�shoz. L�sd a p�ld�ban a for loopot r�. 
-	//Val�sz�n�leg csak el�g egy index hogy h�nyadik kulcs-�rt�k p�rn�l tart. 
-	//Ha k�zben m�dosul a t�mb, nem baj ha a mutat� fura helyekre mutat, ezt megoldani a user dolga. 
-	
+	private List<Entry<Object, Object>> collection = new ArrayList<>();
+	private int iterator=0;
+	private boolean isArray = true; 
+		
 	@Override
 	public boolean asBoolean() {
 		if(!collection.isEmpty())
@@ -32,7 +28,7 @@ public class Collection extends Object {
 		StringBuilder strb = new StringBuilder();
 		strb.append("{ Collection | ");
 		
-		for(Entry<Object, Object> f : collection.entrySet()) 
+		for(Entry<Object, Object> f : collection) 
 			strb.append(f.getKey().toString())
 				.append(": ")
 				.append(f.getValue().toString())
@@ -41,125 +37,159 @@ public class Collection extends Object {
 		return strb.append('}').toString();
 	}
 	
+	private boolean incrementalArraynessValidation(Object newKey) {
+		if(!(newKey instanceof Number)) 
+			return false; 
+		
+		Number nkey = (Number) newKey; 
+		if(nkey.value < 0)
+			return false; 
+		
+		if(nkey.value != (int) nkey.value)
+			return false; 
+		
+		int n = (int) nkey.value; 
+		if(n == 0)
+			return true; 
+		
+		if(!this.has(new Number(n-1)))
+			return false; 
+		
+		return true; 
+	}
+	
 	public void put(Object key, Object value) {
-		// Elteszi key-hez value-t 
-		collection.put(key, value);
+		for(Entry<Object, Object> f : collection) 
+			if(f.getKey().equals(key)) {
+				f.setValue(value);
+				return;
+			}
+		
+		collection.add(new SimpleEntry<Object, Object>(key, value));
+		
+		this.isArray &= incrementalArraynessValidation(key);
 	}
 	
 	public void add(Object value) {
-		// Ugyanazt csin�lja mint assignEmptyIndex
-		
 		int maxVal = -1;
-		for(Map.Entry<Object,Object> entry : collection.entrySet())
+		for(Entry<Object,Object> entry : collection)
 			if(entry.getKey() instanceof Number){ 
 				Number num = (Number) entry.getKey();
 				
 				if(num.value>maxVal)
 					maxVal = (int) ((Number) entry.getKey()).value;
 			}
-				
-		collection.put(new Number(maxVal + 1), value);
+		
+		collection.add(new SimpleEntry<Object,Object>(new Number(maxVal+1), value));
 	}
 	
 	public Object get(Object key) {
-		return collection.get(key); 
+		for(Entry<Object, Object> e: collection)
+			if(e.getKey().equals(key))
+				return e.getValue();
+		
+		return null;
 	}
 	
 	public List<Object> keys() {
-		// Visszaadja a kulcsokat
 		List<Object> returnList= new ArrayList<Object>();
 		
-		for(Map.Entry<Object,Object> entry : collection.entrySet())
+		for(Entry<Object,Object> entry : collection)
 			returnList.add(entry.getKey());
 			
 		return returnList;
 	}
 	
 	public List<Object> values() {
-		// Visszaadja az �rt�keket
 		List<Object> returnList= new ArrayList<Object>();
 		
-		for(Map.Entry<Object,Object> entry : collection.entrySet())
+		for(Entry<Object,Object> entry : collection)
 			returnList.add(entry.getValue());
 		
 		return returnList; 
 	}
 	
 	public int size() { 
-		// Visszaadja hogy h�ny kulcs-�rt�k p�ros van a Collection-ben 
 		return collection.size();
 	}
 	
 	public boolean has(Object key) {
-		// True ha van ilyen kulcs
-		return collection.containsKey(key); 
+		for(Entry<Object, Object> e: collection)
+			if(e.getKey().equals(key))
+				return true;
+		
+		return false; 
 	}
 	
 	public boolean hasValue(Object value) {
-		//True ha van ilyen �rt�k
-		return collection.containsValue(value); 
+		for(Entry<Object, Object> e: collection)
+			if(e.getValue().equals(value))
+				return true;
+		
+		return false; 
 	}
 	
 	public void rewind() {
-		// Vissza�ll�tja a mutat�t null�ra 
 		iterator=0;
 	}
 	
 	public boolean pastEnd() {
-		// True-t ad ha m�r v�gigiter�ltunk a kollekci�n �s most �pp egyel az utols� kulcs-�rt�k p�r ut�n vagyunk. 
-		// Olyan mint C++-ban az end() f�ggv�ny az utols� ut�ni elemre ad iter�tort 
-		if(iterator>collection.size()-1)
+		if(iterator >= collection.size())
 			return true;
 		
 		return false; 
 	}
 	
 	public void next() {
-		// Egyel od�bb rakja a mutat�t 
 		iterator++;
 	}
 	
 	public Object key() {
-		// Visszaadja a kulcsot a mutat� alatt
-		Object key = (Object) collection.keySet().toArray()[iterator];
-
-		return key;
+		return collection.get(iterator).getKey();
 	}
 	
 	public Object value() {
-		// Visszaadja az �rt�ket a mutat� alatt
-		Object key = (Object) collection.keySet().toArray()[iterator];
-		Object value=collection.get(key);
+		return collection.get(iterator).getValue();
+	}
+	
+	public static Collection mergeArrays(Collection... collections) {
+		Collection result = new Collection();
 		
-		return value; 
+		for(Collection c: collections)
+			for(Object o: c.values())
+				result.add(o);
+		
+		return result; 
+	}
+	
+	public static Collection mergeMaps(Collection... collections) {
+		Collection result = new Collection();
+		
+		for(Collection c: collections)
+			for(Entry<Object, Object> e: c.collection)
+				result.put(e.getKey(), e.getValue());
+		
+		return result; 
 	}
 	
 	@Override 
 	public Object accessIndex(Object index) {
-		// Kulcshoz tartoz� �rt�k, vagy UnknownIndex error ha nincs ilyen kulcs. 
-		// Az UnknownIndex-et �rd bele StandardErrors-ba, mint a t�bbi error. 
-		// Kapjon egy self �s egy index mez�t. 
-		if(!has(index))
+		Object result = this.get(index);
+		if(result == null)
 			return StandardErrors.UnknownIndexError(index, this);
 		
-		return collection.get(index);
+		return result;
 	}
 
 	@Override 
 	public Object assignIndex(Object index, Object value) {
-		//Csak sim�n elteszi index-hez value-t 
-		collection.put(index, value);
-		
-		return value;// I dk
+		this.put(index, value);
+		return value; 
 	}
 	
 	@Override 
 	public Object assignEmptyIndex(Object value) {
-		//Ha �res a Collection, adja hozz� Number(0)-s indexszel
-		//Ha nem, keresse meg a legnagyobb Number kulcsot, adjon hozz� egyet �s azzal a kulccsal tegye el
-		//Ha nincs Number kulcs, rakja el Number(0)-val. 
-	
-		add(value);
+		this.add(value);
 		return value;
 	}
 	
@@ -167,27 +197,16 @@ public class Collection extends Object {
 	public Object opAdd(Object rhs) {
 		// Alapj�raton k�pezze a k�t halmaz uni�j�t - csin�ljon egy �j Collection-t, pakolja bele this �sszes elem�t, 
 		// majd rhs-nek azon elemeit amelyek kulcsa m�g nem szerepel az eredm�nyben
-		Collection result = this;
+
+		if(!(rhs instanceof Collection)) 
+			return StandardErrors.IllegalOperation("opAdd", this, rhs);
 		
-		if(rhs instanceof Collection){
-			Collection rColl= (Collection) rhs;
-			if(rColl.isArray()){
-				result=this;
-				for(Map.Entry<Object,Object> entry : rColl.collection.entrySet())
-					result.put(entry.getKey(), entry.getValue());
-			}
-			else
-				for(Map.Entry<Object,Object> entry : rColl.collection.entrySet()){
-					if(result.hasValue(entry.getKey()))
-						result.put(entry.getKey(), entry.getValue());
-			}
-			
-		}else 
-			return StandardErrors.IllegalOperation("opAdd", this);
+		Collection rhsc = (Collection) rhs;
 		
-		// Ha van isArray(), akkor ha t�mb, m�solja le this-t �s f�zze rhs-t a v�g�re
-		
-		return result;
+		if(this.isArray() && rhsc.isArray())
+			return mergeArrays(this, rhsc);
+		else 
+			return mergeMaps(this, rhsc);
 	}
 
 	@Override
@@ -215,61 +234,38 @@ public class Collection extends Object {
 
 	@Override
 	public boolean equals(Object rhs) {
-		// Ha minden kulcs-�rt�k p�r egyezik, true, am�gy false. 
-		if(rhs instanceof Collection){
-			Collection rColl= (Collection)rhs;
-			//THIS
-			for(Map.Entry<Object,Object> entry : rColl.collection.entrySet())
-				if(!collection.containsKey(entry.getKey()) || 
-						!collection.get(entry.getKey()).equals(entry.getValue()))
-					return false;
-			
-			for(Map.Entry<Object,Object> entry : collection.entrySet())
-				if(!rColl.collection.containsKey(entry.getKey()) || 
-						!rColl.collection.get(entry.getKey()).equals(entry.getValue()))
-					return false;
-			
-			return true;
-		}
-			
-		return false;
+		if(!typesMatch(rhs))
+			return false; 
+		
+		Collection rhsc = (Collection) rhs; 
+		
+		if(this.size() != rhsc.size())
+			return false; 
+		
+		for(Entry<Object, Object> e: collection)
+			if(!e.getValue().equals(rhsc.get(e.getKey())))
+				return false;
+		
+		return true; 
 	}
 
 	@Override
 	public boolean lowerThan(Object rhs) {
-		// Rendezzen hossz szerint vagy csak sim�n false mert nincs �rtelmezve. Amelyik szimpi. 
-		if(rhs instanceof Collection){
-			Collection rColl=(Collection) rhs;
-			if(collection.size()<rColl.collection.size())
-				return true;
-		}
+		if(!typesMatch(rhs))
+			return false; 
 		
-		return false;
+		return this.size() < ((Collection) rhs).size();
 	}
 
 	@Override
 	public boolean greaterThan(Object rhs) {
-		// Rendezzen hossz szerint vagy csak sim�n false mert nincs �rtelmezve. Amelyik szimpi.
-		if(rhs instanceof Collection){//azert nem a lowerthant hivom mert lehet egyenlo is
-			Collection rColl=(Collection) rhs;
-			
-			if(collection.size()>rColl.collection.size())
-				return true;
-		}
+		if(!typesMatch(rhs))
+			return false; 
 		
-		return false;
+		return this.size() > ((Collection) rhs).size();
 	}
 
 	public boolean isArray() {
-		// Ha oda�rsz :D Akkor sz�m�t egy Collection t�mbnek, ha a kulcsai csakis term�szetes sz�mok ( nulla belesz�m�tva )
-		// �s nincsen k�zt�k kihagy�s 
-		// Ezt igaz�b�l helyben is le lehet ellen�rizni, de azt is csin�lhatod h tartasz egy boolean v�ltoz�t, �s ha v�ltoztatj�k 
-		// a collection-t, �ll�tod a flag-et. Igaz�b�l csak assignIndex-n�l kell ezt elv�gezni, hogy sz�mot pakolnak-e bele, 
-		// term�szetes sz�m-e, �s hogy az �j indexszel nincs-e kihagy�s. 
-		// Szal ha az n kulcs amire assign-ol, az term�szetes sz�m, akkor meg kell n�zni hogy az nulla, vagy l�tezik-e az n-1 
-		// kulcsra �rt�k. 
-		
-		
-		return false; 
+		return this.isArray; 
 	}
 }
